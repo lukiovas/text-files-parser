26e447f759602c02a8879b7b5433d769905da20a
Lukas_Tuckus <lukas.tuckus@coxautoinc.com>
2022-02-23 09:42:07 +0200

    First commit

---
+++ src/main/java/com/lukas/textfileparser/TextFileParserApplication.java
@@ -0,0 +1,13 @@
+package com.lukas.textfileparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextFileParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextFileParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lukas/textfileparser/TextFileParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukas.textfileparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextFileParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
